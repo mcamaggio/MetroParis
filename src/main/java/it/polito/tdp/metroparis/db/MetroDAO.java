6528f004dd6738d2ed855a9e47e43c3721c99e69
Marco Camaggio <100542819+mcamaggio@users.noreply.github.com>
2022-05-04 18:37:11 +0200

    2022/05/02

--- src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
+++ src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
@@ -10,6 +10,7 @@ import java.util.List;
 import com.javadocmd.simplelatlng.LatLng;
 
 import it.polito.tdp.metroparis.model.Connessione;
+import it.polito.tdp.metroparis.model.CoppiaId;
 import it.polito.tdp.metroparis.model.Fermata;
 import it.polito.tdp.metroparis.model.Linea;
 
@@ -69,6 +70,146 @@ public class MetroDAO {
 		return linee;
 	}
 
+	/**
+	 * Se esiste almeno una connessione tra partenza e arrivo
+	 * @param partenza
+	 * @param arrivo
+	 * @return
+	 */
+	public boolean isFermateConnesse(Fermata partenza, Fermata arrivo) {
+		
+		final String sql = "SELECT COUNT(*) AS cnt "
+				+ "FROM connessione "
+				+ "WHERE id_stazP = ? "
+				+ "AND id_stazA = ?";
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			st.setInt(2, arrivo.getIdFermata());
+			ResultSet rs = st.executeQuery();
+			
+			rs.first();
+			int count = rs.getInt("cnt");
+			st.close();
+			conn.close();
+			
+			return count>0;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Errore di connessione al Database.", e);
+		}
+		
+		
+	}
+
+	public List<Integer> getIdFermateConnesse(Fermata partenza) {
+		
+		final String sql = "SELECT id_stazA "
+				+ "FROM connessione "
+				+ "WHERE id_stazP = ? "
+				+ "GROUP BY id_stazA";
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			ResultSet rs = st.executeQuery();
+			
+			List<Integer> idFermate = new ArrayList<Integer>();
+			
+			while (rs.next()) {
+				idFermate.add(rs.getInt("id_stazA"));
+			}
+			
+			st.close();
+			conn.close();
+			
+			return idFermate;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Errore di connessione al Database.", e);
+		}
+		
+	}
+	
+	public List<Fermata> getFermateConnesse(Fermata partenza){
+		
+		final String sql = "SELECT DISTINCT f.id_fermata, f.nome, f.coordx, f.coordy "
+				+ "FROM fermata f, connessione c "
+				+ "WHERE c.id_stazP = ? AND C.id_stazA = f.id_fermata "
+				+ "ORDER BY f.nome ASC ";
+		
+		final String sql2 = "SELECT id_fermata, nome, coordx, coordy "
+				+ "FROM fermata "
+				+ "WHERE id_fermata IN ( "
+				+ "SELECT id_stazA "
+				+ "FROM connessione "
+				+ "WHERE id_stazP = ? "
+				+ "GROUP BY id_stazA "
+				+ ") "
+				+ "ORDER BY nome ASC ";
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			ResultSet rs = st.executeQuery();
+			
+			List<Fermata> fermate = new ArrayList<Fermata>();
+			
+			while (rs.next()) {
+				Fermata f = new Fermata(rs.getInt("id_Fermata"), rs.getString("nome"),
+						new LatLng(rs.getDouble("coordx"), rs.getDouble("coordy")));
+				fermate.add(f);
+			}
+			
+			st.close();
+			conn.close();
+			
+			return fermate;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Errore di connessione al Database.", e);
+		}
+	}
+	
+	public List<CoppiaId> getAllFermateConnesse(){
+		
+		final String sql = "SELECT DISTINCT id_stazP, id_stazA FROM connessione";
+		
+
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet rs = st.executeQuery();
+			
+			List<CoppiaId> result = new ArrayList<CoppiaId>();
+			
+			while (rs.next()) {
+				CoppiaId c = new CoppiaId(rs.getInt("id_stazP"), rs.getInt("id_stazA"));
+				result.add(c);
+			}
+
+			st.close();
+			conn.close();
+			
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Errore di connessione al Database.");
+		}
+
+		
+		
+	}
+	
+	
 	
 
 }
